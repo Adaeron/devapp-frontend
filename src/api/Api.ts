67c5678e964ab22c4c01dca27637a7ea376c8455
Adaeron <sebastian.milanesi@gmail.com>
2025-05-03 14:27:09 -0300

    add: URL y Puerto para Axios

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+const PORT = 3000;
+const URL = 'devapp-backend.railway.internal';
+
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: `${URL}:${PORT}`
 });
 
 export default api;
