d8288b6f6ac3fc83fe89afbf0819e167b52918f8
Adaeron <sebastian.milanesi@gmail.com>
2025-05-03 20:44:22 -0300

    fix: Correción https en URL backend

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'http://devapp-backend-production.up.railway.app:3000';
+const URL = 'https://devapp-backend-production.up.railway.app:3000';
 
 const api = axios.create({
     baseURL: `${URL}`
