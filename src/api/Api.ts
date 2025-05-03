deb9ad41b44c6d5a88aa3ca4dfc9f87bb012d946
Adaeron <sebastian.milanesi@gmail.com>
2025-05-03 20:41:16 -0300

    fix: Cambio en URL Api

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'devapp-backend-production.up.railway.app';
+const URL = 'http://devapp-backend-production.up.railway.app:3000';
 
 const api = axios.create({
     baseURL: `${URL}`
